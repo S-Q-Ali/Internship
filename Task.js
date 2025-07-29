ca90312ffef0abfba4ee5cbedb64e1610624549d
Syed Qasim Ali <131404679+S-Q-Ali@users.noreply.github.com>
2025-07-29 12:12:33 -0700

    Update Task.js

--- Task.js
+++ Task.js
@@ -106,3 +106,83 @@ function styleAndCelebrate(color, background, fontSize, txt, reason) {
 // Call styleAndCelebrate with appropriate arguments
 // WRITE YOUR CODE HERE
 styleAndCelebrate('#ef7c8e', '#fae8e0', '30px', 'You made it!', 'champions');
+
+
+
+
+'use strict';
+let dice_num_Display = document.querySelector('.dice');
+let roll_button = document.querySelector('.btn--roll');
+let current_score_0 = document.querySelector('#current--0');
+let current_score_1 = document.querySelector('#current--1');
+const reset_game = document.querySelector('.btn--new');
+let score_element_0 = document.querySelector('#score--0');
+let score_element_1 = document.querySelector('#score--1');
+const hold_button = document.querySelector('.btn--hold');
+
+let activePlayer = 0;
+let random;
+let Score_1;
+let Score_2;
+roll_button.addEventListener('click', function () {
+  random = Math.floor(Math.random() * 6) + 1;
+  console.log(random);
+  dice_num_Display.src = `dice-${random}.png`;
+
+  if (activePlayer == 1) {
+    if (random !== 1) {
+      Score_1 = random;
+      current_score_0.textContent = random;
+    } else {
+      current_score_0.textContent = 0;
+      activePlayer = 0;
+    }
+  } else {
+    if (activePlayer == 0) {
+      if (random !== 1) {
+        Score_2 = random;
+        current_score_1.textContent = random;
+      } else {
+        current_score_1.textContent = 0;
+        activePlayer = 1;
+      }
+    } else {
+      console.log('Not Reached!');
+    }
+  }
+});
+
+random = Math.floor(Math.random() * 6) + 1;
+let Total_1=0;
+let Total_2=0;
+hold_button.addEventListener('click', function () {
+
+    if (activePlayer == 1 && Score_1 !== 1) {
+      Total_1 += Score_1;
+      score_element_0.textContent = Total_1;
+      activePlayer = 0;
+      current_score_0.textContent = 0;
+    } else {
+      if (activePlayer == 0 && Score_2 !== 1) {
+        Total_2 += Score_2;
+        score_element_1.textContent = Total_2;
+        activePlayer = 1;
+        current_score_1.textContent = 0;
+      }
+    }
+      if (Total_1 >= 100) {
+    alert('Player 1 Won!');
+  } else if (Total_2 >= 100) {
+    alert('Player 2 Won');
+  } else {
+    console.log("Try Again!");
+  }
+});
+reset_game.addEventListener('click', function () {
+  current_score_0.textContent = 0;
+  current_score_1.textContent = 0;
+  score_element_0.textContent = 0;
+  score_element_1.textContent = 0;
+  dice_num_Display.src = `dice-${random}.png`;
+});
+
